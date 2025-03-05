bad9e857df78b3bc11d195a128fd456ca8b66a00
cerdicwongg <62328771+cerdicwongg@users.noreply.github.com>
2025-03-06 00:25:42 +0800

    updated file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'CerdicWongWeiKit-Capstone/finance-dashboard/',
+  base: '/CerdicWongWeiKit-Capstone/',
 })
